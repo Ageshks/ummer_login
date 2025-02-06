3ea585ebb5da33c4589fcde5d0ee861ab8c16314
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2025-02-06 21:57:59 +0530

    Add files via upload

--- src/main/java/com/example/aquafin/services/ProductService.java
+++ src/main/java/com/example/aquafin/services/ProductService.java
@@ -2,18 +2,23 @@ package com.example.aquafin.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.aquafin.models.Product;
 
+@Service
 public interface ProductService {
 
     void addProduct(Product product);
 
     List<Product> getAllProducts();
 
-    void deleteProduct(Long id);
+    void deleteProductById(Long id);
 
     void updateProduct(Product product);
     
     Product getProductById(Long id);
+
+    // List<Product> getAvailableProducts();
 
 }
